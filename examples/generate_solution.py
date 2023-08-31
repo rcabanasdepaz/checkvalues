2d395c833867db01cff74575039a4b8b3670ca91
Rafael Cabañas <rcabanas@ual.es>
2023-08-31 16:38:02 +0200

    new solutions

--- examples/generate_solution.py
+++ examples/generate_solution.py
@@ -3,7 +3,7 @@ from checkvalues.solution import Solution
 x = 2
 y = 3.0
 z = 4
-lista = ["1", 2, 3.0]
+L = ["1", 2, 3.0]
 
 
 s = Solution()
@@ -11,7 +11,7 @@ s = Solution()
 s.add("Exercise 1", "var", "x", x)
 s.add("Exercise 2", "var", "y", y)
 s.add("Exercise 3", "var", "z", z)
-s.add("Exercise 4", "var", "lista", lista)
+s.add("Exercise 4", "var", "L", L)
 s.add("Exercise 5", "func", "func1", 23.0, args="int:2;float:3", points=0.5)
 s.add("Exercise 5", "func", "func1", 32, kwargs="b=int:2;a=int:3", points=0.5)
 
